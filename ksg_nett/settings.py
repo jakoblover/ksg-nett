55cba6b3e13c3aea0b9c75c59e38531cba900a88
Tormod Haugland <tormod.haugland@gmail.com>
2018-02-09 06:25:51 +0100

    Add a default pagination class for DRF
    This suppresses a warning coming with Django2.0 if such a default is not
    given.

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -154,6 +154,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSIONS_CLASSES': (
         'rest_framework.permissions.IsAdminUser'
     ),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination'
+    ),
     'PAGE_SIZE': 20
 }
 
